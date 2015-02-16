093ad3bfade1b15fac26c193da12be9b7ff9fde4
Jack Lanchantin <jacklanchantin1@gmail.com>
2015-02-15 23:29:13 -0500

    finished db

--- CreateDatabase.py
+++ CreateDatabase.py
@@ -14,8 +14,9 @@ import numpy as np
 import math
 import random
 
+folder = os.getcwd()
 images = {}
-with open(os.path.join(os.getcwd(), 'GroundTruth.txt')) as f:
+with open(os.path.join(folder, 'GroundTruth.txt')) as f:
 	for line in f:
 		l = line.split()
 		if l[0] in images:
@@ -25,41 +26,68 @@ with open(os.path.join(os.getcwd(), 'GroundTruth.txt')) as f:
 			images[l[0]].append(l[1:])
 
 
+faceCount = 0
+nonFaceCount = 0
 imageDB = []
 allImages = os.path.join(os.getcwd(), 'allImages/')
 for f in listdir(allImages):
 	fullImg = skimage.img_as_float(skimage.io.imread(os.path.join(os.getcwd(), 'allImages/')+f))
 	if f in images:
-		lEx,lEy,rEx,rEy,nx,ny,lMx,lMy,cMx,cMy,rMx,rMy = images[f][0] 
-		X = [float(lEx),float(rEx),float(nx),float(lMx),float(cMx),float(rMx)]
-		Y = [float(lEy),float(rEy),float(ny),float(lMy),float(cMy),float(rMy)]
+		# for i in range(0,len(images[f])):
+		if (faceCount < 100):
+			lEx,lEy,rEx,rEy,nx,ny,lMx,lMy,cMx,cMy,rMx,rMy = images[f][0] 
+			X = [float(lEx),float(rEx),float(nx),float(lMx),float(cMx),float(rMx)]
+			Y = [float(lEy),float(rEy),float(ny),float(lMy),float(cMy),float(rMy)]
 
-		minY = min(Y) - 10
-		maxY = max(Y) + 10
-		minX = min(X) - 10
-		maxX = max(X) + 10
+			boxRange = 30
+			minY = min(Y) - boxRange
+			maxY = max(Y) + boxRange
+			minX = min(X) - boxRange
+			maxX = max(X) + boxRange
 
-		face = fullImg[minY:maxY,minX:maxX]
-		face_Resized = scipy.misc.imresize(face,(12,12))
-		imageDB.append(face_Resized)
-	else:
-		random.seed(64)
-		x1 = random.randint(0,fullImg.shape[1])
-		x2 = random.randint(0,fullImg.shape[1])
+			face = fullImg[minY:maxY,minX:maxX]
+			try:
+				face_Resized = scipy.misc.imresize(face,(12,12))
+				imageDB.append(face_Resized)
+				faceCount += 1
+			except:
+				pass
 
-		y1 = random.randint(0,fullImg.shape[0])
-		y2 = random.randint(0,fullImg.shape[0])
+			
 
-		minX = min(x1,x2)
-		maxX = max(x1,x2)
+for f in listdir(allImages):
+	fullImg = skimage.img_as_float(skimage.io.imread(os.path.join(os.getcwd(), 'allImages/')+f))
+	for i in range(0,10):
+		if (nonFaceCount < 100):
+			random.seed(i)
+			x1 = random.randint(0,fullImg.shape[1])
+			x2 = random.randint(0,fullImg.shape[1])
 
-		minY = min(y1,y2)
-		maxY = max(y1,y2)
+			y1 = random.randint(0,fullImg.shape[0])
+			y2 = random.randint(0,fullImg.shape[0])
 
-		notFace = fullImg[minY:maxY,minX:maxX]
-		notFace_Resized = scipy.misc.imresize(notFace,(12,12))
+			minX = min(x1,x2)
+			maxX = max(x1,x2)
 
-		imageDB.append(notFace_Resized)
+			minY = min(y1,y2)
+			maxY = max(y1,y2)
+
+			notFace = fullImg[minY:maxY,minX:maxX]
+			notFace_Resized = scipy.misc.imresize(notFace,(12,12))
+
+			imageDB.append(notFace_Resized)
+			nonFaceCount += 1
+
+collage = np.concatenate(imageDB)
+collage = np.reshape(collage, (120, 240))
+
+skimage.io.imsave(folder + '/collage.png', collage)
+
+
+imageDB = np.array(imageDB)
+
+fMean = np.mean(imageDB[0:100])
+nfMean = np.mean(imageDB[100:200])
 
 
 #plt.imshow(fullImg, cmap = plt.get_cmap('gray')); plt.show()
